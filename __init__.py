d1bc6374a559d36fb08aaf8dcfed7f16ddc73f98
Dushyant Parmar <dushyantp@splunk.com>
2021-07-13 11:59:23 +0530

    add init file and .git* in exclude file

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# File: firesight_connector.py
+#
+# Copyright (c) 2015-2021 Splunk Inc.
+#
+# SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
+# without a valid written license from Splunk Inc. is PROHIBITED.
+#
+# --
